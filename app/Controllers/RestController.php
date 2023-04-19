26cf9375053d8af92dbf350517bcd1e937228ef6
Jorik2018 <antoniopinedo2018@gmail.com>
2023-04-19 11:39:37 -0500

    changes

--- app/Controllers/RestController.php
+++ app/Controllers/RestController.php
@@ -73,7 +73,7 @@ class RestController extends Controller
         $categories=[];
         $brands=[];
         $row=[];
-        return wp_upload_dir()."\DELTRON.csv";
+        return json_encode(wp_upload_dir());
         if (($handle = fopen(wp_upload_dir()."\DELTRON.csv", "r")) !== FALSE) {
             return 11;
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
