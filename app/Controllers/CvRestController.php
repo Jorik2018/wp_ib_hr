8d2b3436956dfa9e74e4f252ff2463ad60e7cb20
Jorik2018 <antoniopinedo2018@gmail.com>
2023-04-27 12:44:35 -0500

    add end points

--- app/Controllers/CvRestController.php
+++ app/Controllers/CvRestController.php
@@ -56,42 +56,42 @@ class CvRestController extends Controller
 
     public function init()
     {
-        register_rest_route( 'api/rh','/employee/(?P<from>\d+)/(?P<to>\d+)', array(
+        register_rest_route( 'api/hr','/employee/(?P<from>\d+)/(?P<to>\d+)', array(
             'methods' => 'GET',
             array($this,'employee_pag')
         ));
 
-        register_rest_route( 'api/rh','/employee/(?P<id>\d+)', array(
+        register_rest_route( 'api/hr','/employee/(?P<id>\d+)', array(
             'methods' => 'GET',
             array($this,'employee_get')
         ));
 
-        register_rest_route( 'api/rh','/study/(?P<from>\d+)/(?P<to>\d+)', array(
+        register_rest_route( 'api/hr','/study/(?P<from>\d+)/(?P<to>\d+)', array(
             'methods' => 'GET',
             array($this,'study_pag')
         ));
 
-        register_rest_route( 'api/rh','/study/(?P<id>\d+)', array(
+        register_rest_route( 'api/hr','/study/(?P<id>\d+)', array(
             'methods' => 'GET',
             array($this,'study_get')
         ));
 
-        register_rest_route( 'api/rh','/training/(?P<from>\d+)/(?P<to>\d+)', array(
+        register_rest_route( 'api/hr','/training/(?P<from>\d+)/(?P<to>\d+)', array(
             'methods' => 'GET',
             array($this,'training_pag')
         ));
 
-        register_rest_route( 'api/rh','/training/(?P<id>\d+)', array(
+        register_rest_route( 'api/hr','/training/(?P<id>\d+)', array(
             'methods' => 'GET',
             array($this,'training_get')
         ));
 
-        register_rest_route( 'api/rh','/experience/(?P<from>\d+)/(?P<to>\d+)', array(
+        register_rest_route( 'api/hr','/experience/(?P<from>\d+)/(?P<to>\d+)', array(
             'methods' => 'GET',
             array($this,'experience_pag')
         ));
 
-        register_rest_route( 'api/rh','/experience/(?P<id>\d+)', array(
+        register_rest_route( 'api/hr','/experience/(?P<id>\d+)', array(
             'methods' => 'GET',
             array($this,'experience_get')
         ));
