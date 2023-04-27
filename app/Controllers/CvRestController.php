770a9d63f6f1028ef930234ce7e2195f966f4f35
Jorik2018 <antoniopinedo2018@gmail.com>
2023-04-27 12:24:29 -0500

    add end points

--- app/Controllers/CvRestController.php
+++ app/Controllers/CvRestController.php
@@ -14,16 +14,86 @@ use WPMVC\MVC\Controller;
 class CvRestController extends Controller
 {
 
-    public function get_employee()
+    public function employee_get()
+    {
+
+    }
+
+    public function employee_pag()
+    {
+
+    }
+
+    public function study_get()
+    {
+
+    }
+
+    public function study_pag()
+    {
+
+    }
+
+    public function training_get()
+    {
+
+    }
+
+    public function training_pag()
+    {
+
+    }
+
+    public function experience_get()
+    {
+
+    }
+
+    public function experience_pag()
     {
 
     }
 
     public function init()
     {
-        register_rest_route( 'api/rh','/employee', array(
+        register_rest_route( 'api/rh','/employee/(?P<from>\d+)/(?P<to>\d+)', array(
             'methods' => 'GET',
-            array($this,'get_employee')
+            array($this,'employee_pag')
+        ));
+
+        register_rest_route( 'api/rh','/employee/(?P<id>\d+)', array(
+            'methods' => 'GET',
+            array($this,'employee_get')
+        ));
+
+        register_rest_route( 'api/rh','/study/(?P<from>\d+)/(?P<to>\d+)', array(
+            'methods' => 'GET',
+            array($this,'study_pag')
+        ));
+
+        register_rest_route( 'api/rh','/study/(?P<id>\d+)', array(
+            'methods' => 'GET',
+            array($this,'study_get')
+        ));
+
+        register_rest_route( 'api/rh','/training/(?P<from>\d+)/(?P<to>\d+)', array(
+            'methods' => 'GET',
+            array($this,'training_pag')
+        ));
+
+        register_rest_route( 'api/rh','/training/(?P<id>\d+)', array(
+            'methods' => 'GET',
+            array($this,'training_get')
+        ));
+
+        register_rest_route( 'api/rh','/experience/(?P<from>\d+)/(?P<to>\d+)', array(
+            'methods' => 'GET',
+            array($this,'experience_pag')
+        ));
+
+        register_rest_route( 'api/rh','/experience/(?P<id>\d+)', array(
+            'methods' => 'GET',
+            array($this,'experience_get')
         ));
     }
     
