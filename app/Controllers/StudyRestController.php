4a7d85ce7bcc45e3d21eba4a3cc5f344cdc79e43
Jorik2018 <antoniopinedo2018@gmail.com>
2023-04-29 22:52:02 -0500

    split in multiple controller

--- app/Controllers/StudyRestController.php
+++ app/Controllers/StudyRestController.php
@@ -71,7 +71,8 @@ class StudyRestController extends Controller
 
     public function get($request){    
         global $wpdb;
-        $o = $wpdb->get_row($wpdb->prepare("SELECT * FROM hr_study WHERE id=" . $request['id']), ARRAY_A);
+        $o = (object)$wpdb->get_row($wpdb->prepare("SELECT * FROM hr_study WHERE id=" 
+            . $request['id']), ARRAY_A);
         if ($wpdb->last_error) return t_error();
         return Util\toCamelCase($o);
     }
