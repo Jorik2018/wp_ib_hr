e5aa98f7bcc2b1eb64d5b0af0772aa2b6877109e
Jorik2018 <antoniopinedo2018@gmail.com>
2023-04-29 20:54:16 -0500

    split in multiple controller

--- app/Main.php
+++ app/Main.php
@@ -4,6 +4,25 @@ namespace IB\cv;
 
 use WPMVC\Bridge;
 
+function toCamelCase($data) {
+    if (is_array($data)) {
+        $result = array();
+        foreach ($data as $item) {
+            $result[] = toCamelCase($item);
+        }
+        return $result;
+    } elseif (is_object($data)) {
+        $result = new stdClass();
+        foreach ($data as $key => $value) {
+            $newKey = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $key))));
+            $result->$newKey = toCamelCase($value);
+        }
+        return $result;
+    } else {
+        return $data;
+    }
+}
+
 class Main extends Bridge
 {
     public function api_covid(){
