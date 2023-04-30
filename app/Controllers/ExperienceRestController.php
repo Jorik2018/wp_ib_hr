070b3555491bfcab8ac72e0d29496045bcba872d
Jorik2018 <antoniopinedo2018@gmail.com>
2023-04-29 20:35:54 -0500

    split in multiple controller

--- app/Controllers/ExperienceRestController.php
+++ app/Controllers/ExperienceRestController.php
@@ -69,7 +69,7 @@ class ExperienceRestController extends Controller
         global $wpdb;
         $o = $wpdb->get_row($wpdb->prepare("SELECT * FROM hr_experience WHERE id=" . $request['id']), ARRAY_A);
         if ($wpdb->last_error) return t_error();
-        return IB\cv\toCamelCase($o);
+        return Util\toCamelCase($o);
     }
 
     public function pag($request){
