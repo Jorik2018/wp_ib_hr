56354ea54e0766d534697079679f3bbe6ba88a99
Jorik2018 <antoniopinedo2018@gmail.com>
2023-06-07 18:32:57 -0500

    x

--- app/Controllers/EmployeeRestController.php
+++ app/Controllers/EmployeeRestController.php
@@ -87,8 +87,8 @@ class EmployeeRestController extends Controller
     public function get($request){    
         global $wpdb;
         $o = $wpdb->get_row($wpdb->prepare("SELECT * FROM hr_employee WHERE id=" . $request['id']), ARRAY_A);
-		if(intval($o['people_id'])){
-			$meta_values = get_user_meta(1*$o['people_id'], array('first_name', 'last_name'), false);
+		if(isset($o['people_id'])){
+			$meta_values = get_user_meta(intval($o['people_id']), array('first_name', 'last_name'), false);
 			$o['meta_values']=$meta_values;
 			if (!empty($meta_values)) {
 				$o['names']=$meta_values[0];
