d5a4eea008aa2ae370a3335f8207e2535cd9156a
Jorik2018 <antoniopinedo2018@gmail.com>
2024-07-01 19:32:50 -0500

    x

--- app/Controllers/RestController.php
+++ app/Controllers/RestController.php
@@ -68,7 +68,7 @@ class RestController extends Controller
                 add_post_meta($post->ID, '_thumbnail_id',$attachment_id,true);
                 return $post;
             }else{
-                return array('uri'=>$uri,'found':0);
+                return array('uri'=>$uri,'found'=>false);
             }
         }
 
