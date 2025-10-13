dd09947801af73847a9f98d6a2262e6cbfa0db91
jorik2018 <antoniopinedo2018@gmail.com>
2025-10-13 14:35:39 -0500

    x

--- app/Controllers/ServiceRestController.php
+++ app/Controllers/ServiceRestController.php
@@ -133,6 +133,8 @@ class ServiceRestController extends Controller
         $current_user = wp_get_current_user();
         $db_erp = get_option("db_erp");
         $db_erp = "bwgvinpi_ofis";
+        $people = $wpdb->get_row($wpdb->prepare("SELECT * FROM $db_erp.m_personal WHERE n=%s", $personal), ARRAY_A);
+        return $people;
         $wpdb->last_error = '';
         $results = $wpdb->get_results("SELECT SQL_CALC_FOUND_ROWS em.* FROM $db_erp.t_servicios em " .
             "WHERE 1=1 " . (isset($query) ? " AND (pe.apellidos_nombres LIKE '%$query%') " : "") .
