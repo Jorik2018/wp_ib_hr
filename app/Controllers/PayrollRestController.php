7e2cc76507d767eff9deefba941368c69a22e0a1
jorik2018 <antoniopinedo2018@gmail.com>
2026-03-08 11:42:03 -0500

    x

--- app/Controllers/PayrollRestController.php
+++ app/Controllers/PayrollRestController.php
@@ -912,7 +912,8 @@ class PayrollRestController extends Controller
             'x' => 12,
             'headers' => $headers,
             'payroll' => $payroll,
-            '$amountMap' => $amountMap
+            '$amountMap' => $amountMap,
+            '$concepts' => $concepts
         ];
     }
 
