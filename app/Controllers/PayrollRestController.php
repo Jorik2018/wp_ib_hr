2a7ca749af9c8edcf318c0f79baf489f79db079b
jorik2018 <antoniopinedo2018@gmail.com>
2026-03-08 13:33:54 -0500

    x

--- app/Controllers/PayrollRestController.php
+++ app/Controllers/PayrollRestController.php
@@ -804,7 +804,7 @@ class PayrollRestController extends Controller
         );*/
         $employees = $wpdb->get_results(
             $wpdb->prepare(
-                "SELECT p.apellidos_nombres fullName, pp.payroll_type_id, pp.people_id, p.afp_onp pensionSystem, p.n_cussp nCUSSP
+                "SELECT p.apellidos_nombres fullName, pp.payroll_type_id, pp.people_id, p.afp_onp pensionSystem, p.n_cuspp nCUSSP
          FROM rem_payroll_type_people pp
          INNER JOIN m_personal p ON p.n = pp.people_id
          ORDER BY 1 ",
