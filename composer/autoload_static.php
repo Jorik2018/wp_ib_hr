6a09c8d22f2d293af7652ce2b156a32dbcaff519
jorik2018 <antoniopinedo2018@gmail.com>
2026-03-09 12:41:53 -0500

    x

--- composer/autoload_static.php
+++ composer/autoload_static.php
@@ -526,6 +526,7 @@ class ComposerStaticInitab41187644036ee6d526b14dd1cb815f
         'Dompdf\\FrameReflower\\Page' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameReflower/Page.php',
         'Dompdf\\FrameReflower\\Block' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameReflower/Block.php',
         'Dompdf\\FrameReflower\\Text' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameReflower/Text.php',
+        'Dompdf\\FrameReflower\\Table' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameReflower/Table.php',
         'Dompdf\\FrameReflower\\AbstractFrameReflower' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameReflower/AbstractFrameReflower.php',
         'Dompdf\\FrameDecorator\\AbstractFrameDecorator' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameDecorator/AbstractFrameDecorator.php',
         'Dompdf\\FrameDecorator\\Block' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameDecorator/Block.php',
