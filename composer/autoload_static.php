6d3fecc21b922ec4497f94f4fe9130c5d27ce227
jorik2018 <antoniopinedo2018@gmail.com>
2026-03-09 14:30:13 -0500

    x

--- composer/autoload_static.php
+++ composer/autoload_static.php
@@ -537,6 +537,7 @@ class ComposerStaticInitab41187644036ee6d526b14dd1cb815f
         'Dompdf\\FrameDecorator\\TableRowGroup' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameDecorator/TableRowGroup.php',
         'Dompdf\\FrameDecorator\\TableRow' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/FrameDecorator/TableRow.php',
         'Dompdf\\Positioner\\Block' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/Positioner/Block.php',
+        'Dompdf\\Positioner\\TableCell' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/Positioner/TableCell.php',
         'Dompdf\\Positioner\\NullPositioner' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/Positioner/NullPositioner.php',
         'Dompdf\\Positioner\\Inline' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/Positioner/Inline.php',
         'Dompdf\\Positioner\\AbstractPositioner' => __DIR__ . '/../../vendor' . '/dompdf/dompdf/src/Positioner/AbstractPositioner.php',
